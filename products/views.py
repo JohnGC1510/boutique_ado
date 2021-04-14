a066d913e83ad539a96ca47e16af2ec05f1b9c61
JohnGC1510 <johngc1510@gmail.com>
2021-04-14 09:21:37 +0000

    Added search functionallity

--- products/views.py
+++ products/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
 def all_products(request):
-    """ A  view to show all products """
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            """ Q allows search between both products and descritpion, these are usally coupled together """
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            """ i makes query case insensetive """
+            """ with Q command query filter would only give products where query shows in both the product name and description """
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
