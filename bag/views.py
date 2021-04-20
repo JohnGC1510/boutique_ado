d7645bb7d0d24c8fb308cfaaa18c015247ae5687
JohnGC1510 <johngc1510@gmail.com>
2021-04-20 21:16:11 +0000

    Added qunaitiy input button to products

--- bag/views.py
+++ bag/views.py
@@ -12,15 +12,27 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     # create or get session variable
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        # if item in bag update quanity
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        # if item not in bag add to bag
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            # if item in bag update quanity
+            bag[item_id] += quantity
+        else:
+            # if item not in bag add to bag
+            bag[item_id] = quantity
 
     # overwrite session varaible with updated version of bag
     request.session['bag'] = bag
