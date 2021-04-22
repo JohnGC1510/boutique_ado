8cb10fd3ff2e40a1a6a5010e19a48e01a68ba9ec
JohnGC1510 <johngc1510@gmail.com>
2021-04-22 20:40:30 +0000

    Product Admi - add product view, url and template

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail')
-
+    # specify product id as integer othwerwise add will be interpeted
+    # as a product id as django won't be able to tell the differnece
+    # between a product number and string like add
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product')
 ]
