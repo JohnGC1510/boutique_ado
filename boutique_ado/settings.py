79cfc24839b8c6d6ed8240c4367fdb9b01a51fcb
JohnGC1510 <johngc1510@gmail.com>
2021-04-23 12:19:04 +0000

    IMporved cache

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -184,6 +184,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 20999 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'johngc-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
