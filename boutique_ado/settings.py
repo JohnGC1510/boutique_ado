43a2bad687c9c1ae66bdc517d5b87ddc27e7c912
JohnGC1510 <johngc1510@gmail.com>
2021-04-23 12:11:25 +0000

    changed aws region name

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -185,7 +185,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'johngc-boutique-ado'
-    AWS_S3_REGION_NAME = 'EU (london)'
+    AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
